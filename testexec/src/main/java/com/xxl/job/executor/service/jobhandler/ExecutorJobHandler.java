61bb00a56d89eda07c6d6785ae55bd37b5242296
sunguangyuan <guangyuan.sun@medbanks.cn>
2020-03-14 15:38:16 +0800

    用brach

--- testexec/src/main/java/com/xxl/job/executor/service/jobhandler/ExecutorJobHandler.java
+++ testexec/src/main/java/com/xxl/job/executor/service/jobhandler/ExecutorJobHandler.java
@@ -96,13 +96,9 @@ public class ExecutorJobHandler  {
                 errThread.interrupt();
             }
 //            //  删除临时文件
-            if (FileUtil.exist(tmpFilePath)) {
-                FileUtil.del(new File(tmpFilePath));
-                System.out.println("fjja");
-                System.out.println("fjja");
-                System.out.println("fjja");
-                System.out.println("fjja");
-            }
+//            if (FileUtil.exist(tmpFilePath)) {
+//                FileUtil.del(new File(tmpFilePath));
+//            }
         }
         if (exitValue == 0) {
             return IJobHandler.SUCCESS;
